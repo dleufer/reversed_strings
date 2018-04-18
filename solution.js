7334f5f13f06cc50451a9b24127d45dfc1e8379c
dleufer <dleufer@gmail.com>
2018-04-18 17:08:06 +0200

    add test case and solutions

--- solution.js
+++ solution.js
@@ -1,3 +1,5 @@
 // Your code here
-
+function solution(str){
+  return str.split("").reverse().join("");
+}
 // Be sure it's the cleanest code you can write!
--- tests/test-cases.js
+++ tests/test-cases.js
@@ -2,8 +2,20 @@ tests({
 
   // Fill this in with your test cases
 
-  '': function() {
-    eq("", your_solution(""));
+  'tibbar': function() {
+    eq("rabbit", your_solution("tibbar"));
+  }
+
+  'mauT': function() {
+    eq("Tuam", your_solution("mauT"));
+  }
+
+  'leffunk': function() {
+    eq("knuffel", your_solution("leffunk"));
+  }
+
+  'neopmop': function() {
+    eq("pompoen", your_solution("neopmop"));
   }
 
 });
